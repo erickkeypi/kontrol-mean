8124874bbfbf9e723dd4109f5b5bd91074c26f09
erick garcia <erickrgm24@gmail.com>
2019-04-01 17:41:50 -0400

    hechas la pantalla principal

--- src/app/home-kontrol/home.component.ts
+++ src/app/home-kontrol/home.component.ts
@@ -2,7 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-screen',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styles: [`
+    p {
+      display: flex;
+      text-align: center;
+    }
+
+    .space{
+      flex: 1 1 auto;
+    }
+
+    div {
+      display: flex;
+      flex-direction: column;
+      width: 90%;
+      margin: 40px auto;
+    }
+
+    div button{
+      margin-top: 20px;
+      font-size: 20px;
+    }
+  `]
 })
 
-export class HomeKontrolComponent {}
+export class HomeKontrolComponent {
+
+}
